debf17fdd5c18302388842f89dbe63006c906010
DaniilPaskal <50429826+DaniilPaskal@users.noreply.github.com>
2022-12-29 23:29:50 -0500

    Initial commit

---
+++ src/components/DBFunctions.jsx
@@ -0,0 +1,51 @@
+import { useEffect, useState } from 'react';
+import { collection, doc, query, getDocs, addDoc, setDoc, updateDoc } from 'firebase/firestore';
+import { db } from '../firebase';
+
+export function QueryCats(table, predicate) {
+    const [cats, setCats] = useState();
+    const attributes = {name:'', collar:'', colour:'', date:'', adj:'', sex:'', father:'', mother:''};
+    const q = query(collection(db, table));
+    
+    const fetchCats = async () => {
+        await getDocs(collection(db, table))
+            .then((querySnapshot)=>{
+                const fetchedData = querySnapshot.docs.map((doc) => ({
+                    ...doc.data(), id:doc.id
+                }))
+                setCats(fetchedData);
+            }).then((cats) => {
+                console.log(cats);
+                return cats;
+            });
+    };
+
+    useEffect(()=>{
+        fetchCats();
+    }, []);
+}
+
+export function InsertCat(table, cat) {
+    const { name, collar, colour, sex, adj, date, cattery, location, mother, father } = cat;
+    var id = name;
+
+    if (date) {
+        id = id + '.' + date;
+    }
+
+    const addCat = async () => {
+        const docRef = doc(db, table, id);
+
+        await setDoc(docRef, {});
+    
+        Object.keys(cat).map(async (key) => {
+            await updateDoc(docRef, {
+                [key]: cat[key]
+            })
+        });
+    };
+
+    useEffect(()=>{
+        addCat();
+    }, []);
+}
