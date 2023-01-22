5efb33e0e930b9d9825339010218302abd7c49f9
DaniilPaskal <50429826+DaniilPaskal@users.noreply.github.com>
2023-01-21 19:36:12 -0500

    Started form fields

--- src/pages/AddCats.jsx
+++ src/pages/AddCats.jsx
@@ -1,6 +1,17 @@
 
 const AddCats = () => {
-    
+    <div className='sign-up-container'>
+        <h2>Add new cat to database:</h2>
+        <form onSubmit={handleSubmit}>
+            <FormInput label='Name' required onChange={handleChange} name='name' value={name}/>
+            <FormInput label='Date' required onChange={handleChange} name='date' value={date}/>
+            
+                
+            <div className='buttons-container'>
+                <Button type='submit'>Submit</Button>
+            </div>
+        </form>
+    </div>
 }
 
 export default AddCats;
