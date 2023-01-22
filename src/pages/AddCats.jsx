700509006075db18d98de29e8135e027fe1bc5c8
DaniilPaskal <50429826+DaniilPaskal@users.noreply.github.com>
2023-01-22 17:26:44 -0500

    Worked on AddCat form

--- src/pages/AddCats.jsx
+++ src/pages/AddCats.jsx
@@ -1,17 +1,35 @@
 
 const AddCats = () => {
-    <div className='sign-up-container'>
-        <h2>Add new cat to database:</h2>
-        <form onSubmit={handleSubmit}>
-            <FormInput label='Name' required onChange={handleChange} name='name' value={name}/>
-            <FormInput label='Date' required onChange={handleChange} name='date' value={date}/>
-            
+    const [formFields, setFormFields] = useState('');
+    const { name, date, sex, adj, colour, status, father, mother, cattery, location } = formFields;
+    
+    const resetFormFields = () => {
+        setFormFields('');
+    }
+
+    const handleChange = (event) => {
+        const { name, value } = event.target;
+        setFormFields({...formFields, [name]: value});
+    }
+
+    const handleSubmit = async (event) => {
+
+    }
+
+    return (
+        <div className='sign-up-container'>
+            <h2>Add new cat to database:</h2>
+            <form onSubmit={handleSubmit}>
+                <FormInput label='Name' required onChange={handleChange} name='name' value={name}/>
+                <FormInput label='Date' required onChange={handleChange} name='date' value={date}/>
                 
-            <div className='buttons-container'>
-                <Button type='submit'>Submit</Button>
-            </div>
-        </form>
-    </div>
+                    
+                <div className='buttons-container'>
+                    <Button type='submit'>Submit</Button>
+                </div>
+            </form>
+        </div>
+    );
 }
 
 export default AddCats;
