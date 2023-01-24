0fa3ccd95057090344cf0482b12f16e3baf094ea
DaniilPaskal <50429826+DaniilPaskal@users.noreply.github.com>
2023-01-24 01:34:03 -0500

    Changed body from string to array

--- src/components/AccordionMenu.jsx
+++ src/components/AccordionMenu.jsx
@@ -4,34 +4,37 @@ import './../App.css';
 
 const AccordionMenu = ({ items }) => {
   var itemID = -1;
-  var title = '';
-  var body = '';
 
   return (
     <>
       {items.length > 0 &&
       <Accordion className='accordion' alwaysOpen flush>
         {items.map((item) => {
+          var title = '';
+          var body = [];
           itemID++;
 
           if (item.name) {
               title = item.name;
               
               Object.keys(item).map((key) => {
-                  body += `${key}: ${item.key}\n`;  
+                  body.push(`${key}: ${item[key]}`);  
               });
           }
 
           return (
-              <Accordion.Item className='accordion-item' eventKey={itemID}>
+              <Accordion.Item className='accordion-item' key={itemID} eventKey={itemID}>
                   <Accordion.Header className='accordion-header'>
                     <p className='accordion-header-text'>
                       {title}
                     </p>
                   </Accordion.Header>
                   <Accordion.Body>
-                    {body}
-
+                    {body.map((line) => {
+                      return (
+                        <p>{line}<br /></p>
+                      );
+                    })}
                     <button type='edit'>Edit</button>
                   </Accordion.Body>
               </Accordion.Item>
