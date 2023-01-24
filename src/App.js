f7ce73542353f4e9db495cb02893a913cc28dbba
DaniilPaskal <50429826+DaniilPaskal@users.noreply.github.com>
2023-01-23 22:20:45 -0500

    Fixed an import

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import { Routes, Route } from 'react-router-dom';
-import Home from './pages/home';
+import Home from './pages/Home';
 import AddCats from './pages/AddCats';
 import ViewCats from './pages/ViewCats';
 import News from './pages/News';
