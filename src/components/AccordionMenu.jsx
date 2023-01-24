95f76d1850bb47cf078c6518640c56670a34571e
DaniilPaskal <50429826+DaniilPaskal@users.noreply.github.com>
2023-01-24 00:17:31 -0500

    Reworked AccordionMenu to display cats

--- src/components/AccordionMenu.jsx
+++ src/components/AccordionMenu.jsx
@@ -4,26 +4,41 @@ import './../App.css';
 
 const AccordionMenu = ({ items }) => {
   var itemID = -1;
+  var title = '';
+  var body = '';
 
   return (
+    <>
+    <h1>oh</h1>
     <Accordion className='accordion' alwaysOpen flush>
       {items.map((item) => {
         itemID++;
+
+        if (item.name) {
+            title = item.name;
+            
+            Object.keys(item).map((key) => {
+                body += `${key}: ${item.key}\n`;  
+            });
+        }
 
         return (
             <Accordion.Item className='accordion-item' eventKey={itemID}>
                 <Accordion.Header className='accordion-header'>
                   <p className='accordion-header-text'>
-                    {item.title}
+                    {title}
                   </p>
                 </Accordion.Header>
                 <Accordion.Body>
-                  {item.body}
+                  {body}
+
+                  <button type='edit'>Edit</button>
                 </Accordion.Body>
             </Accordion.Item>
         )
       })}
     </Accordion>
+    </>
   );
 };
 
