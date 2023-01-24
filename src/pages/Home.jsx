f681469500c92ac3bbd5db7158d66b18c7d71134
DaniilPaskal <50429826+DaniilPaskal@users.noreply.github.com>
2023-01-23 22:47:51 -0500

    Added links to pages

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,6 +1,23 @@
+import AddCats from "./AddCats";
+import ViewCats from "./ViewCats";
+import News from "./News";
 
 const Home = () => {
-    
+    return (
+        <div className='page-content'>
+            <ul>
+                <li>
+                    <Link to='/add-cats'>Add cats</Link>
+                </li>
+                <li>
+                    <Link to='/view-cats'>View cats</Link>
+                </li>
+                <li>
+                    <Link to='/news'>News</Link>
+                </li>
+            </ul>
+        </div>
+    )
 }
 
 export default Home;
