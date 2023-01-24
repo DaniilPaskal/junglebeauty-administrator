ad29006a2dd20e756312991c57dced87f48c1738
DaniilPaskal <50429826+DaniilPaskal@users.noreply.github.com>
2023-01-24 01:34:22 -0500

    Got cat accordions to work

--- src/pages/ViewCats.jsx
+++ src/pages/ViewCats.jsx
@@ -1,6 +1,31 @@
+import { useState, useEffect } from "react";
+import { QueryCats } from "../components/DBFunctions";
+import AccordionMenu from "../components/AccordionMenu";
 
 const ViewCats = () => {
+    const [parents, setParents] = useState([]);
+    const [kittens, setKittens] = useState([]);
+
+    const getCats = async () => {
+        const parents = await QueryCats('parents');
+        const kittens = await QueryCats('kittens');
+        setParents(parents);
+        setKittens(kittens);
+    }
     
+    useEffect(() => {
+        getCats();
+    }, []);
+
+    return (
+        <>
+            <h2>Parents:</h2>
+            <AccordionMenu items={parents} />
+
+            <h2>Kittens:</h2>
+            <AccordionMenu items={kittens} />
+        </>
+    );
 }
 
-export default ViewCats;
+export default ViewCats;  
