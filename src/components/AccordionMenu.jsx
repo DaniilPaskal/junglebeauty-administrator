7bf6dfa0b11a944e28e6c8c9d20a384237a8deb9
DaniilPaskal <50429826+DaniilPaskal@users.noreply.github.com>
2023-01-24 23:34:27 -0500

    Changed AccordionMenu to display all possible fields for all cats

--- src/components/AccordionMenu.jsx
+++ src/components/AccordionMenu.jsx
@@ -8,40 +8,48 @@ const AccordionMenu = ({ items }) => {
   return (
     <>
       {items.length > 0 &&
-      <Accordion className='accordion' alwaysOpen flush>
-        {items.map((item) => {
-          var title = '';
-          var body = [];
-          itemID++;
+        <Accordion className='accordion' alwaysOpen flush>
+          {items.map((item) => {
+            var title = '';
+            var body = '';
+            itemID++;
 
-          if (item.name) {
-              title = item.name;
-              
-              Object.keys(item).map((key) => {
-                  body.push(`${key}: ${item[key]}`);  
-              });
-          }
+            if (item.name) {
+                title = item.name;
+                
+                body = `
+                  id: ${item.id}
+                  ${item.mother ? `collar:` : `name:`}: ${item.name}
+                  birthdate: ${item.date}
+                  colour: ${item.colour}
+                  adjective: ${item.adj}
+                  mother: ${item.mother}
+                  father: ${item.father}
+                  birth location: ${item.location}
+                  cattery: ${item.cattery}
+                `
+            }
 
-          return (
-              <Accordion.Item className='accordion-item' key={itemID} eventKey={itemID}>
-                  <Accordion.Header className='accordion-header'>
-                    <p className='accordion-header-text'>
-                      {title}
-                    </p>
-                  </Accordion.Header>
-                  <Accordion.Body>
-                    {body.map((line) => {
-                      return (
-                        <p>{line}<br /></p>
-                      );
-                    })}
-                    <button type='edit'>Edit</button>
-                  </Accordion.Body>
-              </Accordion.Item>
-          )
-        })}
-      </Accordion>
-    }
+            return (
+                <Accordion.Item className='accordion-item' key={itemID} eventKey={itemID}>
+                    <Accordion.Header className='accordion-header'>
+                      <p className='accordion-header-text'>
+                        {title}
+                      </p>
+                    </Accordion.Header>
+                    <Accordion.Body>
+                      {body.map((line) => {
+                        return (
+                          <p>{line}<br /></p>
+                        );
+                      })}
+                      <button type='edit'>Edit</button>
+                    </Accordion.Body>
+                </Accordion.Item>
+            )
+          })}
+        </Accordion>
+      }
     </>
   );
 };
