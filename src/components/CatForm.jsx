34e0db3201fe5d1e9861f866a34933fb0673fbf1
DaniilPaskal <50429826+DaniilPaskal@users.noreply.github.com>
2023-01-26 00:00:03 -0500

    Created CatForm component

---
+++ src/components/CatForm.jsx
@@ -0,0 +1,13 @@
+import { useState } from 'react';
+import { Modal } from 'react-bootstrap';
+import './../App.css';
+
+const CatForm = ({ cat }) => {
+    
+    return (
+        <>
+        </>
+    );
+};
+
+export default CatForm;
