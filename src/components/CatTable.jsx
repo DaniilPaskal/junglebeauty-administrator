2914b7241e31a79db02763130b054818a80f8abc
DaniilPaskal <50429826+DaniilPaskal@users.noreply.github.com>
2023-01-25 23:17:04 -0500

    Got cats to display in table

--- src/components/CatTable.jsx
+++ src/components/CatTable.jsx
@@ -4,10 +4,28 @@ import './../App.css';
 
 const CatTable = ({ cats }) => {
   
-
   return (
     <>
-      
+      <Table striped bordered hover>
+        <thead>
+          <tr>
+            <th>ID</th>
+            <th>Name/Collar</th>
+            <th>Birthdate</th>
+          </tr>
+        </thead>
+        <tbody>
+          {cats.map((cat) => {
+            return (
+              <tr key={cat.id}>
+                <td>{cat.id}</td>
+                <td>{cat.name}</td>
+                <td>{cat.date || `N/A`}</td>
+              </tr>
+            );
+          })}
+        </tbody>
+      </Table>
     </>
   );
 };
