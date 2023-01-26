a3b435a8f1eee2c40f209cf5a92311966041bb8e
DaniilPaskal <50429826+DaniilPaskal@users.noreply.github.com>
2023-01-25 23:06:02 -0500

    Created CatTable component

---
+++ src/components/CatTable.jsx
@@ -0,0 +1,15 @@
+import { useContext } from 'react';
+import { Table } from 'react-bootstrap';
+import './../App.css';
+
+const CatTable = ({ cats }) => {
+  
+
+  return (
+    <>
+      
+    </>
+  );
+};
+
+export default CatTable;
