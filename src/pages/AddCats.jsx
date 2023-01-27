4e994d0df422f29f739b3cde84ee3448dc4404d9
DaniilPaskal <50429826+DaniilPaskal@users.noreply.github.com>
2023-01-26 23:48:52 -0500

    Changed AddCats to return CatForm

--- src/pages/AddCats.jsx
+++ src/pages/AddCats.jsx
@@ -1,37 +1,10 @@
 import { useState, useContext } from "react";
+import CatForm from "../components/CatForm";
 
 const AddCats = () => {
-    const [formFields, setFormFields] = useState('');
-    const [type, setType] = useState('parent');
-    const { name, date, sex, adj, colour, status, father, mother, cattery, location } = formFields;
-    
-    const resetFormFields = () => {
-        setFormFields('');
-    }
-
-    const handleChange = (event) => {
-        const { name, value } = event.target;
-        setFormFields({...formFields, [name]: value});
-    }
-
-    const handleSubmit = async (event) => {
-
-    }
-
     return (
-        <div className='sign-up-container'>
-            <h2>Add new cat to database:</h2>
-            <form onSubmit={handleSubmit}>
-                <label>
-                    
-                </label>
-                    
-                <div className='buttons-container'>
-                    <button type='submit'>Submit</button>
-                </div>
-            </form>
-        </div>
-    );
+        <CatForm />
+    )
 }
 
 export default AddCats;
