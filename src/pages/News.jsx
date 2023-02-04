88a150acdb7bd5b5e82e9be439e23c024bd9a8be
DaniilPaskal <50429826+DaniilPaskal@users.noreply.github.com>
2023-02-04 17:58:36 -0500

    Started working on news page

--- src/pages/News.jsx
+++ src/pages/News.jsx
@@ -1,6 +1,17 @@
 
 const News = () => {
-    
+    <Table striped bordered hover>
+        <thead>
+          <tr>
+            <th>ID</th>
+            <th>Posted</th>
+            <th>Title</th>
+          </tr>
+        </thead>
+        <tbody>
+          
+        </tbody>
+      </Table>
 }
 
 export default News;
