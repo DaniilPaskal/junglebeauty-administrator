196d9eb24d7ad2a482098b4be7ee0df8fe879f3a
DaniilPaskal <50429826+DaniilPaskal@users.noreply.github.com>
2023-02-08 22:48:39 -0500

    Created login form

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,8 +1,24 @@
+import { useState } from 'react';
 
 const Login = () => {
+    const [user, setUser] = useState();
+
     return (
         <div>
+            <form>
+                <label>
+                    Email:
+                    <input name='email' type='email' onChange={handleChange} />
+                </label>
+                <label>
+                    Password:
+                    <input name='password' type='password' onChange={handleChange} />
+                </label>
 
+                <div className='buttons-container'>
+                    <button type='submit' onClick={handleLogin}>Log in</button>
+                </div>
+            </form>
         </div>
     )
 }
