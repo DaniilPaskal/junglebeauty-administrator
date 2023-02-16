167e7048a47e43d48da0d6a03fc1e369b934f9d1
DaniilPaskal <50429826+DaniilPaskal@users.noreply.github.com>
2023-02-16 18:42:45 -0500

    Added key prop to images in ImageCarousel

--- src/components/CatImage.jsx
+++ src/components/CatImage.jsx
@@ -4,6 +4,8 @@ import { GetImage } from './DBFunctions';
 const CatImage = ({ filepath }) => {
     const [image, setImage] = useState();
 
+    console.log(filepath);
+
     const getImage = async () => {
         const image = await GetImage(filepath);
         setImage(image);
