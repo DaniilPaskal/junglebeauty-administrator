f5f0d584f654537a5edc1f10c7c68b365f124d3f
DaniilPaskal <50429826+DaniilPaskal@users.noreply.github.com>
2023-02-22 00:06:19 -0500

    Added AuthProvider function

--- src/contexts/AuthContext.jsx
+++ src/contexts/AuthContext.jsx
@@ -2,6 +2,14 @@ import { useContext } from "react";
 
 const AuthContext = React.createContext();
 
+export function AuthProvider({children, value}) {
+    return (
+        <AuthContext.Provider value={value}>
+            {children}
+        </AuthContext.Provider>
+    );
+}
+
 export function useAuth() {
     return useContext(AuthContext);
 }
