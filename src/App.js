d92d6133b586481f741d06f9490a3a9a5befc1b1
DaniilPaskal <50429826+DaniilPaskal@users.noreply.github.com>
2023-02-22 00:08:51 -0500

    Wrapped App in AuthProvider

--- src/App.js
+++ src/App.js
@@ -1,4 +1,6 @@
 import { Routes, Route } from 'react-router-dom';
+import { useState } from 'react';
+import { AuthProvider } from './contexts/AuthContext';
 import Home from './pages/Home';
 import Login from './pages/Login';
 import AddCats from './pages/AddCats';
@@ -8,19 +10,23 @@ import Navigation from './components/Navigation';
 import './App.css';
 
 function App() {
+  const [user, setUser] = useState(null);
+
   return (
-    <div className="App">
-      <Navigation />
-      <div className='page-content'>
-        <Routes>
-          <Route index element={<Home />} />
-          <Route path='login' element={<Login />} />
-          <Route path='add-cats' element={<AddCats />} />
-          <Route path='view-cats' element={<ViewCats />} />
-          <Route path='news' element={<News />} />
-        </Routes>
+    <AuthProvider value={user}>
+      <div className="App">
+        <Navigation />
+        <div className='page-content'>
+          <Routes>
+            <Route index element={<Home />} />
+            <Route path='login' element={<Login />} />
+            <Route path='add-cats' element={<AddCats />} />
+            <Route path='view-cats' element={<ViewCats />} />
+            <Route path='news' element={<News />} />
+          </Routes>
+        </div>
       </div>
-    </div>
+    </AuthProvider>
   );
 }
 
