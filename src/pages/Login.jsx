863a389482f5db05830f67b0506143de13289d39
DaniilPaskal <50429826+DaniilPaskal@users.noreply.github.com>
2023-03-02 17:23:05 -0500

    Got authentication working

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,24 +1,27 @@
-import { signInWithEmailAndPassword } from 'firebase/auth';
 import { useState } from 'react';
+import { Navigate, useNavigate } from 'react-router-dom';
+import { signInWithEmailAndPassword } from 'firebase/auth';
 import { auth } from '../firebase';
 
 const Login = () => {
-    const [user, setUser] = useState();
+    const [user, setUser] = useState({email: '', password: ''});
     const { email, password } = user;
+    const navigate = useNavigate();
 
     const handleChange = (event) => {
         const { name, value } = event.target;
         setUser({ ...user, [name]: value });
     }
 
-    const handleLogin = async (event) => {
-        event.preventDefault();
-
-        try {
-            
-        } catch(error) {
-
-        }
+    const handleLogin = async (e) => {
+        e.preventDefault();
+        signInWithEmailAndPassword(auth, email, password)
+            .then(() => {
+                navigate('/');
+            })
+            .catch((error) => {
+                console.log(error.code);
+            })
     }
 
     return (
