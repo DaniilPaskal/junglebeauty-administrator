f83b7e0b96ac7761ed929081be64a7bd087e46ae
DaniilPaskal <50429826+DaniilPaskal@users.noreply.github.com>
2023-03-02 22:48:52 -0500

    Created handleLogout function

--- src/components/DBFunctions.jsx
+++ src/components/DBFunctions.jsx
@@ -1,7 +1,8 @@
 import { useEffect, useState } from 'react';
 import { collection, doc, query, where, getDocs, addDoc, setDoc, updateDoc, deleteDoc } from 'firebase/firestore';
-import { getAuth, signInWithRedirect, signInWithPopup, GoogleAuthProvider, createUserWithEmailAndPassword, signInWithEmailAndPassword,signOut,onAuthStateChanged} from 'firebase/auth';
+import { auth, getAuth, signOut, signInWithRedirect, signInWithPopup, GoogleAuthProvider, createUserWithEmailAndPassword, signInWithEmailAndPassword,signOut,onAuthStateChanged} from 'firebase/auth';
 import { ref, getStorage, getDownloadURL, listAll } from 'firebase/storage';
+import { useNavigate } from 'react-router-dom';
 import { db } from '../firebase';
 
 export async function QueryCats(table, predicate = []) {
@@ -122,4 +123,14 @@ export async function GetText(textName) {
 export async function SignIn(email, password) {
     const auth = getAuth();
     return await signInWithEmailAndPassword(auth, email, password);
+}
+
+export function handleLogout() {
+    const navigate = useNavigate();
+
+    signOut(auth).then(() => {
+        navigate('login');
+    }).catch((error) => {
+        console.error();
+    });
 }
