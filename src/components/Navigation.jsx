9f1f71ace21e5d0a542383bb19784829cff7688a
DaniilPaskal <50429826+DaniilPaskal@users.noreply.github.com>
2023-03-02 22:42:12 -0500

    Added Accounts link

--- src/components/Navigation.jsx
+++ src/components/Navigation.jsx
@@ -1,9 +1,12 @@
 import React from 'react';
 import { Link, NavLink } from "react-router-dom";
 import { Container, Nav, Navbar, NavDropdown } from 'react-bootstrap';
+import { useAuth } from '../contexts/AuthContext';
 import './../App.css';
 
 const Navigation = () => {
+    const user = useAuth();
+
     return (
         <Navbar collapseOnSelect className='nav-bar' expand="lg" variant='dark' bg='dark'>
             <Container>
@@ -14,7 +17,14 @@ const Navigation = () => {
                         <NavLink className='nav-link' to='/add-cats'>Add Cats</NavLink>
                         <NavLink className='nav-link' to='/view-cats'>View Cats</NavLink>
                         <NavLink className='nav-link' to='/news'>News</NavLink>
+                        <NavLink className='nav-link' to='/accounts' end>Accounts</NavLink>
                     </Nav>
+                    {user &&
+                        <Nav>
+                            
+                            <NavLink className='nav-link' to='/' end>Home</NavLink>
+                        </Nav>
+                    }
                 </Navbar.Collapse>
             </Container>
         </Navbar>
