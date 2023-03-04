2f67ead8a7c5c14c555620b62720ed93fa01290a
DaniilPaskal <50429826+DaniilPaskal@users.noreply.github.com>
2023-03-03 23:14:42 -0500

    Merged Register page into Accounts page

--- src/pages/Accounts.jsx
+++ src/pages/Accounts.jsx
@@ -1,27 +1,19 @@
 import { useState } from 'react';
-import { Navigate, useNavigate } from 'react-router-dom';
-import { signInWithEmailAndPassword } from 'firebase/auth';
-import { auth } from '../firebase';
 
 const Accounts = () => {
-    const [user, setUser] = useState({email: '', password: ''});
-    const { email, password } = user;
-    const navigate = useNavigate();
+    const [user, setUser] = useState();
 
     const handleChange = (event) => {
         const { name, value } = event.target;
         setUser({ ...user, [name]: value });
     }
 
-    const handleLogin = async (e) => {
-        e.preventDefault();
-        signInWithEmailAndPassword(auth, email, password)
-            .then(() => {
-                navigate('/');
-            })
-            .catch((error) => {
-                console.log(error.code);
-            })
+    const validatePassword = () => {
+
+    }
+
+    const handleRegister = () => {
+        
     }
 
     return (
@@ -36,8 +28,13 @@ const Accounts = () => {
                     <input name='password' type='password' onChange={handleChange} />
                 </label>
 
+                <label>
+                    Confirm password:
+                    <input name='confirmPassword' type='password' onChange={handleChange} />
+                </label>
+
                 <div className='buttons-container'>
-                    <button type='submit' onClick={handleLogin}>Log in</button>
+                    <button type='submit' onClick={handleRegister}>Register</button>
                 </div>
             </form>
         </div>
