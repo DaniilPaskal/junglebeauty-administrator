80c5f1ef5848649e2ef847117ec273469e03037f
DaniilPaskal <50429826+DaniilPaskal@users.noreply.github.com>
2023-03-08 15:16:55 -0500

    Applied form CSS

--- src/components/CatForm.jsx
+++ src/components/CatForm.jsx
@@ -18,8 +18,6 @@ const defaultCat = {
 }
 
 const CatForm = ({ cat = defaultCat }) => {
-    const oldName = cat.name;
-    const oldDate = cat.date;
     const [newCat, setNewCat] = useState(cat);
     const { name, date, sex, adj, colour, status, father, mother, cattery, location } = newCat;
     const [type, setType] = useState(mother ? 'kitten' : 'parent');
@@ -54,8 +52,9 @@ const CatForm = ({ cat = defaultCat }) => {
             <form>
                 {cat == defaultCat &&
                     <div>
-                        <label>
+                        <label className='form-label'>
                             Cat type:
+                            <br />
                             <select name='type' value={type} onChange={(e) => setType(e.target.value)}>
                                 <option value='kitten'>Kitten</option>
                                 <option value='parent'>Parent</option>
@@ -65,26 +64,30 @@ const CatForm = ({ cat = defaultCat }) => {
                 }
                     
                 <div>
-                    <label>
+                    <label className='form-label'>
                         Name:
+                        <br />
                         <input name='name' type='text' defaultValue={name} onChange={handleChange} />
                     </label>
                     <br />
-                    <label>
+                    <label className='form-label'>
                         Date of birth:
+                        <br />
                         <input name='date' type='date' defaultValue={date} onChange={handleChange} />
                     </label>
                     <br />
-                    <label>
+                    <label className='form-label'>
                         Sex:
+                        <br />
                         <select name='sex' defaultValue={sex} onChange={handleChange}>
                             <option value='male'>Male</option>
                             <option value='female'>Female</option>
                         </select>
                     </label>
                     <br />
-                    <label>
+                    <label className='form-label'>
                         Status:
+                        <br />
                         <select name='status' defaultValue={status} onChange={handleChange}>
                             {type == 'kitten'
                             ?
@@ -102,13 +105,15 @@ const CatForm = ({ cat = defaultCat }) => {
                         </select>
                     </label>
                     <br />
-                    <label>
+                    <label className='form-label'>
                         Descriptor:
+                        <br />
                         <input name='adj' type='text' defaultValue={adj} onChange={handleChange} />
                     </label>
                     <br />
-                    <label>
+                    <label className='form-label'>
                         Fur colour:
+                        <br />
                         <select name='colour' defaultValue={colour} onChange={handleChange}>
                             <option value='silver'>Silver</option>
                             <option value='brown'>Brown</option>
@@ -120,14 +125,17 @@ const CatForm = ({ cat = defaultCat }) => {
                     {type == 'kitten'
                     ?
                     <>
-                        <label>
+                        <label className='form-label'>
                             Mother:
+                            <br />
                             <select name='mother' defaultValue={mother || ''} onChange={handleChange}>
                                 
                             </select>
                         </label>
-                        <label>
+                        <br />
+                        <label className='form-label'>
                             Father:
+                            <br />
                             <select name='father' defaultValue={father || ''} onChange={handleChange}>
                                 
                             </select>
@@ -135,12 +143,15 @@ const CatForm = ({ cat = defaultCat }) => {
                     </>
                     :
                     <>
-                        <label>
+                        <label className='form-label'>
                             Cattery:
+                            <br />
                             <input name='cattery' type='text' defaultValue={cattery || ''} onChange={handleChange} />
                         </label>
-                        <label>
+                        <br />
+                        <label className='form-label'>
                             Location:
+                            <br />
                             <input name='location' type='text' defaultValue={location || ''} onChange={handleChange} />
                         </label>
                     </>
