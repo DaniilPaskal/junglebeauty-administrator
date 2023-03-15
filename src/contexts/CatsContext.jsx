8a6dc0855bbb49f1cf72c64f2aef44de47fe60df
DaniilPaskal <50429826+DaniilPaskal@users.noreply.github.com>
2023-03-14 23:36:15 -0400

    Created CatsContext for storing cats in a context

---
+++ src/contexts/CatsContext.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { useContext } from "react";
+
+const CatsContext = React.createContext();
+
+export function CatsProvider({ children, value }) {
+    return (
+        <CatsContext.Provider value={value}>
+            {children}
+        </CatsContext.Provider>
+    );
+}
