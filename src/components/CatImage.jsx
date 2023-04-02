1f173ee1329c1f8a1ab275b94a0ac84569148a01
DaniilPaskal <50429826+DaniilPaskal@users.noreply.github.com>
2023-04-01 23:01:10 -0400

    Added image modal

--- src/components/CatImage.jsx
+++ src/components/CatImage.jsx
@@ -1,8 +1,12 @@
 import { useState, useEffect } from 'react';
+import { Modal } from 'react-bootstrap';
 import { GetImage } from './FirebaseFunctions';
 
 const CatImage = ({ filepath }) => {
     const [image, setImage] = useState();
+    const [show, setShow] = useState(false);
+    const handleClose = () => setShow(false);
+    const handleShow = () => setShow(true);
 
     const getImage = async () => {
         const image = await GetImage(filepath);
@@ -13,8 +17,22 @@ const CatImage = ({ filepath }) => {
         getImage();
       }, [])
 
+    const handleDelete = async () => {
+      
+    }
+
     return (
-        <img className='cat-img' src={image}/>
+        <>
+            <img className='cat-img' src={image} onClick={handleShow}/>
+
+            <Modal show={show} onHide={handleClose} size='lg'>
+                <Modal.Header closeButton/>
+                <Modal.Body>
+                  <img className='cat-img' src={image}/>
+                  <button onClick={handleDelete}>Delete</button>
+                </Modal.Body>
+            </Modal>
+        </>
     );
 }
 
