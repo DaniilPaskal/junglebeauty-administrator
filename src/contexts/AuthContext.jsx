0b9526c590a30025a91b9bb18fbf3910c513d3da
DaniilPaskal <50429826+DaniilPaskal@users.noreply.github.com>
2023-04-06 23:54:04 -0400

    Added code for setting user

--- src/contexts/AuthContext.jsx
+++ src/contexts/AuthContext.jsx
@@ -1,11 +1,20 @@
-import React from "react";
-import { useContext } from "react";
+import React, { useContext, useState, useEffect } from "react";
+import { onAuthStateChanged } from "firebase/auth";
+import { auth } from "../firebase";
 
 const AuthContext = React.createContext();
 
-export function AuthProvider({ children, value }) {
+export function AuthProvider({ children }) {
+    const [user, setUser] = useState(null);
+ 
+    useEffect(() => {
+        onAuthStateChanged(auth, (user) => {
+        setUser(user);
+        })
+    }, []);
+
     return (
-        <AuthContext.Provider value={value}>
+        <AuthContext.Provider value={user}>
             {children}
         </AuthContext.Provider>
     );
