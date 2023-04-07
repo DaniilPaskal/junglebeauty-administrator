7a0bce2573db5123b698e0e3058c7ade565c9016
DaniilPaskal <50429826+DaniilPaskal@users.noreply.github.com>
2023-04-06 23:33:17 -0400

    Added code for querying cats to cat context

--- src/contexts/CatsContext.jsx
+++ src/contexts/CatsContext.jsx
@@ -1,9 +1,25 @@
-import React from "react";
+import { React, useState, useEffect } from "react";
 import { useContext } from "react";
+import { QueryCats } from "../components/FirebaseFunctions";
 
 const CatsContext = React.createContext();
 
 export function CatsProvider({ children, value }) {
+    const [parents, setParents] = useState(null);
+    const [kittens, setKittens] = useState(null);
+
+    const getCats = async () => {
+        const parents = await QueryCats('parents');
+        const kittens = await QueryCats('kittens');
+        
+        setParents(parents);
+        setKittens(kittens);
+    }
+    
+    useEffect(() => {
+        getCats();
+    }, []);
+
     return (
         <CatsContext.Provider value={value}>
             {children}
