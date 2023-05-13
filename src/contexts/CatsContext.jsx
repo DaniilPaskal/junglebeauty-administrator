86b47fbdaf57e6ad147cedaf8e24aad720d192f5
DaniilPaskal <50429826+DaniilPaskal@users.noreply.github.com>
2023-05-12 23:11:55 -0400

    Fixed error where null cats variable is passed to components

--- src/contexts/CatsContext.jsx
+++ src/contexts/CatsContext.jsx
@@ -4,13 +4,15 @@ import { QueryCats } from "../components/FirebaseFunctions";
 const CatsContext = React.createContext();
 
 export function CatsProvider({ children }) {
-    const [cats, setCats] = useState(null);
+    const [cats, setCats] = useState([]);
+    const [loading, setLoading] = useState(true);
 
     const getCats = async () => {
         const parents = await QueryCats('parents');
         const kittens = await QueryCats('kittens');
         
-        setCats({parents, kittens})
+        setCats({parents, kittens});
+        setLoading(false);
     }
     
     useEffect(() => {
@@ -19,7 +21,7 @@ export function CatsProvider({ children }) {
 
     return (
         <CatsContext.Provider value={cats}>
-            {children}
+            { !loading && children }
         </CatsContext.Provider>
     );
 }
