9e31247b7bf321063e7d983459cab2315c490310
DaniilPaskal <50429826+DaniilPaskal@users.noreply.github.com>
2023-08-04 23:17:10 -0400

    Filled out news form

--- src/components/NewsForm.jsx
+++ src/components/NewsForm.jsx
@@ -1,8 +1,12 @@
 import { useState } from 'react';
+import { useCats } from '../contexts/CatsContext';
 import './../App.css';
 
 const NewsForm = ({ item }) => {
     const [newItem, setNewItem] = useState(text);
+    var { title, body, date, type, king, queen } = item;
+    const kings = cats.parents.filter((cat) => cat.sex == 'male');
+    const queens = cats.parents.filter((cat) => cat.sex == 'female');
 
     const handleChange = (event) => {
         const { name, value } = event.target;
@@ -24,7 +28,55 @@ const NewsForm = ({ item }) => {
     return (
         <div className='form-container'>
             <form>
+                <label className='form-label'>
+                    Title:
+                    <br />
+                    <input name='title' type='text' defaultValue={title} onChange={handleChange} />
+                </label>
+                <br />
+                <label className='form-label'>
+                    Body:
+                    <br />
+                    <textarea name='body' defaultValue={body} onChange={handleChange} />
+                </label>
+                <br />
+                <label className='form-label'>
+                    Date:
+                    <br />
+                    <input name='date' type='date' defaultValue={date} onChange={handleChange} />
+                </label>
                 
+                <label className='form-label'>
+                    Type:
+                    <br />
+                    <select name='type' defaultValue={type} onChange={handleChange}>
+                        <option value='litter'>Litter</option>
+                    </select>
+                </label>
+                <br />
+                <label className='form-label'>
+                    Father: {king}
+                    <br />
+                    <select name='king' defaultValue={king} onChange={handleChange}>
+                        {kings.map((cat) => {
+                            return (
+                                <option value={cat.name} key={cat.id}>{cat.name}</option>
+                            );
+                        })}
+                    </select>
+                </label>
+                <br />
+                <label className='form-label'>
+                    Queen: {mother}
+                    <br />
+                    <select name='queen' defaultValue={queen} onChange={handleChange}>
+                        {queens.map((cat) => {
+                            return (
+                                <option value={cat.name} key={cat.id}>{cat.name}</option>
+                            );
+                        })}
+                    </select>
+                </label>
 
                 <div className='buttons-container'>
                     <button type='button' onClick={handleUpdate}>Update item</button>
