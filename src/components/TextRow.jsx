df9fb1f6af841f48b9b5bce61a47c2d5654670af
DaniilPaskal <50429826+DaniilPaskal@users.noreply.github.com>
2023-08-04 20:55:43 -0400

    Got text row working for news items

--- src/components/TextRow.jsx
+++ src/components/TextRow.jsx
@@ -3,23 +3,29 @@ import { Modal } from 'react-bootstrap';
 import TextForm from './TextForm';
 import './../App.css';
 
-const TextRow = ({ text }) => {
+const TextRow = ({ item }) => {
     const [show, setShow] = useState(false);
     const handleClose = () => setShow(false);
     const handleShow = () => setShow(true);
 
+    console.log(item)
+
     return (
         <>
             <tr className='cat-row' onClick={handleShow}>
-                <td>{text}</td>
+                {Object.keys(item).map((key) => {
+                    return (
+                        <td>{item[key]}</td>
+                    );
+                })}
             </tr>
 
-            <Modal show={show} onHide={handleClose} size='lg'>
+            {/* <Modal show={show} onHide={handleClose} size='lg'>
                 <Modal.Header closeButton/>
                 <Modal.Body>
                     <TextForm text={text} />
                 </Modal.Body>
-            </Modal>
+            </Modal> */}
         </>
     );
 };
