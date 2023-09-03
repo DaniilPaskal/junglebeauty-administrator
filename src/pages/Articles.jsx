c321e0be43e5aaddec8445c063ae50bc4ade48db
DaniilPaskal <50429826+DaniilPaskal@users.noreply.github.com>
2023-09-03 00:51:20 -0400

    Replaced table with ListTable

--- src/pages/Articles.jsx
+++ src/pages/Articles.jsx
@@ -12,36 +12,7 @@ const Articles = () => {
   const handleShow = () => setShow(true);
 
   return (
-    <>
-      <button type='button' onClick={handleShow}>Add articles item</button>
-      <Table striped bordered hover>
-          <thead>
-            <tr>
-              {Object.keys(articles[0]).map((key) => {
-                return (
-                    <th key={key}>{key}</th>
-                );
-              })}
-            </tr>
-          </thead>
-          <tbody>
-            {articles &&
-              articles.map((articlesItem) => {
-                return (
-                  <TextRow item={articlesItem} key={articlesItem.id} />
-                );
-              })
-            }
-          </tbody>
-        </Table>
-
-        <Modal show={show} onHide={handleClose} size='lg'>
-            <Modal.Header closeButton/>
-            <Modal.Body>
-                <ArticlesForm />
-            </Modal.Body>
-        </Modal>
-      </>
+    <ListTable list={articles} type='articles' />
   );
 }
 
