6ded012f62ff68d11f71d0b5590f68505861e739
DaniilPaskal <50429826+DaniilPaskal@users.noreply.github.com>
2023-09-03 00:50:59 -0400

    Created component for tables of list items

---
+++ src/components/ListTable.jsx
@@ -0,0 +1,48 @@
+import { Table, Modal } from "react-bootstrap";
+import { useState } from "react";
+import TextRow from "../components/TextRow";
+import NewsForm from "../components/NewsForm";
+import ArticlesForm from "./ArticlesForm";
+import './../App.css';
+
+const ListTable = ({ list, type }) => {
+    const [show, setShow] = useState(false);
+    const handleClose = () => setShow(false);
+    const handleShow = () => setShow(true);
+
+    return (
+        <>
+            <button type='button' onClick={handleShow}>Add list item</button>
+            <Table striped bordered hover>
+                <thead>
+                    <tr>
+                    {Object.keys(list[0]).map((key) => {
+                        return (
+                            <th key={key}>{key}</th>
+                        );
+                    })}
+                    </tr>
+                </thead>
+                <tbody>
+                    {list &&
+                    list.map((listItem) => {
+                        return (
+                        <TextRow item={listItem} key={listItem.id} />
+                        );
+                    })
+                    }
+                </tbody>
+            </Table>
+
+            <Modal show={show} onHide={handleClose} size='lg'>
+                <Modal.Header closeButton/>
+                <Modal.Body>
+                    {type === 'news' && <NewsForm />}
+                    {type === 'articles' && <ArticlesForm />}
+                </Modal.Body>
+            </Modal>
+        </>
+    );
+};
+
+export default ListTable;
