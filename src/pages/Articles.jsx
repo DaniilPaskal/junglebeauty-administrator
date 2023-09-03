97a243161cf24babdec74a4d8ce68898e07b3c01
DaniilPaskal <50429826+DaniilPaskal@users.noreply.github.com>
2023-09-03 00:51:48 -0400

    Removed unused code

--- src/pages/Articles.jsx
+++ src/pages/Articles.jsx
@@ -1,15 +1,8 @@
-import { Table, Modal } from "react-bootstrap";
-import { useState } from "react";
 import { useLists } from "../contexts/ListsContext";
-import TextRow from "../components/TextRow";
-import ArticlesForm from "../components/ArticlesForm";
 import './../App.css';
 
 const Articles = () => {
   const articles = useLists().articles;
-  const [show, setShow] = useState(false);
-  const handleClose = () => setShow(false);
-  const handleShow = () => setShow(true);
 
   return (
     <ListTable list={articles} type='articles' />
