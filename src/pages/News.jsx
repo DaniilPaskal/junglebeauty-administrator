645a6c0c6873fa0b10324febbc74ff7d5b66facf
DaniilPaskal <50429826+DaniilPaskal@users.noreply.github.com>
2023-09-03 00:51:27 -0400

    Replaced table with ListTable

--- src/pages/News.jsx
+++ src/pages/News.jsx
@@ -3,6 +3,7 @@ import { useState } from "react";
 import { useLists } from "../contexts/ListsContext";
 import TextRow from "../components/TextRow";
 import NewsForm from "../components/NewsForm";
+import ListTable from "../components/ListTable";
 import './../App.css';
 
 const News = () => {
@@ -12,36 +13,7 @@ const News = () => {
   const handleShow = () => setShow(true);
 
   return (
-    <>
-      <button type='button' onClick={handleShow}>Add news item</button>
-      <Table striped bordered hover>
-          <thead>
-            <tr>
-              {Object.keys(news[0]).map((key) => {
-                return (
-                    <th key={key}>{key}</th>
-                );
-              })}
-            </tr>
-          </thead>
-          <tbody>
-            {news &&
-              news.map((newsItem) => {
-                return (
-                  <TextRow item={newsItem} key={newsItem.id} />
-                );
-              })
-            }
-          </tbody>
-        </Table>
-
-        <Modal show={show} onHide={handleClose} size='lg'>
-            <Modal.Header closeButton/>
-            <Modal.Body>
-                <NewsForm />
-            </Modal.Body>
-        </Modal>
-      </>
+    <ListTable list={news} type='news' />
   );
 }
 
