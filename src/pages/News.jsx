9d449f85f677ec57124091a7e4f889b5e0270933
DaniilPaskal <50429826+DaniilPaskal@users.noreply.github.com>
2023-09-03 00:52:30 -0400

    Removed unused code

--- src/pages/News.jsx
+++ src/pages/News.jsx
@@ -1,16 +1,9 @@
-import { Table, Modal } from "react-bootstrap";
-import { useState } from "react";
 import { useLists } from "../contexts/ListsContext";
-import TextRow from "../components/TextRow";
-import NewsForm from "../components/NewsForm";
 import ListTable from "../components/ListTable";
 import './../App.css';
 
 const News = () => {
   const news = useLists().news;
-  const [show, setShow] = useState(false);
-  const handleClose = () => setShow(false);
-  const handleShow = () => setShow(true);
 
   return (
     <ListTable list={news} type='news' />
