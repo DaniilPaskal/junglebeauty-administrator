837e042c6b5f0a23c1435a1fc7961da529da177f
DaniilPaskal <50429826+DaniilPaskal@users.noreply.github.com>
2023-09-02 20:45:13 -0400

    Created unified context for holding lists

---
+++ src/contexts/ListsContext.jsx
@@ -0,0 +1,31 @@
+import React, { useContext, useState, useEffect } from "react";
+import { getList } from "../components/FirebaseFunctions";
+
+const ListsContext = React.createContext();
+
+export function ListsProvider({ children }) {
+    const [lists, setLists] = useState({});
+    const [loading, setLoading] = useState(true);
+
+    const getLists = async () => {
+        const news = await getList('News-List.json');
+        const articles = await getList('Articles-List.json');
+        
+        setLists({news, articles});
+        setLoading(false);
+    }
+    
+    useEffect(() => {
+        getLists();
+    }, []);
+
+    return (
+        <ListsContext.Provider value={lists}>
+            { !loading && children }
+        </ListsContext.Provider>
+    );
+}
+
+export function useLists() {
+    return useContext(ListsContext);
+}
