a83376c10a5cecaf5f2faac336eea227ec848005
DaniilPaskal <50429826+DaniilPaskal@users.noreply.github.com>
2023-12-11 18:42:34 -0500

    Set settings to kitten filters

--- src/contexts/SettingsContext.jsx
+++ src/contexts/SettingsContext.jsx
@@ -1,12 +1,28 @@
 import React, { useContext, useState, useEffect } from "react";
+import { useCats } from "./CatsContext";
 
 const SettingsContext = React.createContext();
 
 export function SettingsProvider({ children }) {
-    const [settings, setSettings] = useState([]);
+    const [filters, setFilters] = useState({colour: [], father: [], mother: [], status: [], sex: []});
+
+    useEffect(() => {
+        const parents = useCats().parents;
+        const colours = ['silver', 'brown'];
+        const statuses = ['available', 'reserved', 'graduated'];
+        const sexes = ['male', 'female'];
+
+        setFilters({
+            colour: colours,
+            father: parents.filter((cat) => cat.sex === 'male').map((cat) => {return cat.name}),
+            mother: parents.filter((cat) => cat.sex === 'female').map((cat) => {return cat.name}),
+            status: statuses,
+            sex: sexes
+        });
+    }, []);
 
     return (
-        <SettingsContext.Provider value={settings}>
+        <SettingsContext.Provider value={filters}>
             { children }
         </SettingsContext.Provider>
     );
